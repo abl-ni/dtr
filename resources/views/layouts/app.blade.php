840f2d205bdc5d26bb167276bd29e5762e728302
abl-ni <al@bywave.com.au>
2017-12-22 14:08:50 +0800

    time()

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     header("Cache-Control: private, must-revalidate, max-age=0");
     header("Pragma: no-cache");
     header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // A date in the past
+
+    echo time();
 ?>
 <head>
     <meta charset="utf-8">
