a65425257a1ee87dcb8a0fb1da8ef2fea41dd983
abl-ni <al@bywave.com.au>
2018-01-27 01:07:58 +0800

    socket

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -54,5 +54,5 @@ import Echo from 'laravel-echo'
 
 window.Echo = new Echo({
     broadcaster: 'socket.io',
-    host: window.location.hostname + ':6001'
+    host: 'https://timewave-socket.herokuapp.com/'
 });
